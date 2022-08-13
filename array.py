e7f689d76bb7ab7d9db0e3e016c0434a08edfdcc
kavandalal <kavandalal003@gmail.com>
2022-08-14 00:44:15 +0530

    insertion and shell sort

--- array.py
+++ array.py
@@ -1,4 +1,4 @@
-# Bubble sort start 
+# Bubble sort start
 def bubble_sort(a):
     length = len(a)
     for i in range(length - 1):
@@ -6,12 +6,15 @@ def bubble_sort(a):
         for j in range(length-1 - i):
             if a[j] > a[j+1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
-                swapped= True
-        if not swapped: break
+                swapped = True
+        if not swapped:
+            break
     return a
-# Bubble sort end 
+# Bubble sort end
 
-# Binary Search start  
+# Binary Search start
+
+
 def binary_search(a, low, high, x):
     if high >= low:
         mid = (high + low) // 2
@@ -26,6 +29,8 @@ def binary_search(a, low, high, x):
 # Binary Search end
 
 # Quick sort start
+
+
 def quick_sort_partition(arr, start, end):
     pivot = arr[start]
     low = start + 1
@@ -42,6 +47,7 @@ def quick_sort_partition(arr, start, end):
     arr[start], arr[high] = arr[high], arr[start]
     return high
 
+
 def quick_sort(a, low, high):
     if low < high:
         pi = quick_sort_partition(a, low, high)
@@ -50,6 +56,8 @@ def quick_sort(a, low, high):
 # Quick sort end
 
 # Merge Sort start
+
+
 def merge_sort(a):
     if len(a) > 1:
         m = len(a)//2
@@ -79,9 +87,44 @@ def merge_sort(a):
 # Merge Sort End
 
 
+# Insertion Sort Start
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        anchor = a[i]
+        j = i - 1
+        while j >= 0 and anchor < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = anchor
+# LINKS https://www.youtube.com/watch?v=K0zTIF3rm9s&list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&index=16
+# Insertion Sort End
+
+# Shell Sort Start
+
+
+def shell_sort(a):
+    length = len(a)
+    gap = length // 2
+    while gap > 0:
+        for i in range(gap, length):
+            anchor = a[i]
+            j = i
+            while j >= gap and a[j-gap] > anchor:
+                a[j] = a[j-gap]
+                j -= gap
+            a[j] = anchor
+        gap = gap // 2
+# LINKS https://www.youtube.com/watch?v=VxNr9Vudp4Y&list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&index=18
+# Shell Sort End
+
+
 a = [5, 7, 3, 4, 67, 7, 4, 3, 6, 89, 9, 78, 7, 56, 4,
      23, 12, 45, 56, 45, 523234, 6, 345, 56, 345, 45645]
 n = len(a)
 x = 563
-merge_sort(a)
+# merge_sort(a)
+# quick_sort(a)
+# bubble_sort(a)
+# insertion_sort(a)
+shell_sort(a)
 print(a)
