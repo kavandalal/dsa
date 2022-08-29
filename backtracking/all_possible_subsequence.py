ad748d00dad3c1d99bc419c67097fdbf6bb49c42
kavandalal <kavandalal003@gmail.com>
2022-08-29 12:04:39 +0530

    change

--- backtracking/all_possible_subsequence.py
+++ backtracking/all_possible_subsequence.py
@@ -11,6 +11,7 @@ class Solution:
     def __init__(self):
         self.solve_store = []
 
+    # just print all the occurances
     def solve_store_print(self):
         # sorting by keeping thr length of the list into consideration
         self.solve_store.sort(key=len)
@@ -31,7 +32,7 @@ class Solution:
         # not taking the element in the answer Arr
         self.solve_return(i + 1, arr, ansArr)
 
-    # just print all the occurances
+    # print all the subsequence, no need to store it
     def solve_print(self, i, arr, ansArr=[]):
         if i >= len(arr):
             print(ansArr)
@@ -44,12 +45,62 @@ class Solution:
         # not taking the element in the answer Arr
         self.solve_print(i + 1, arr, ansArr)
 
+    # find all the subsequence which has desired sum
+    def find_subseq_sum(self, i, arr, total, ansArr=[]):
+        # print('+', ansArr)
+        if i >= len(arr):
+            if sum(ansArr) == total:
+                print(ansArr)
+        if i < len(arr):
+            ansArr.append(arr[i])
+            self.find_subseq_sum(i + 1, arr,  total, ansArr)
+            ansArr.pop()
+            self.find_subseq_sum(i + 1, arr,  total, ansArr)
+
+    # find only the first summation subsequence and then stop
+    def find_subseq_sum_only_1(self, i, arr, total, ansArr=[]):
+        if i >= len(arr):
+            if sum(ansArr) == total:
+                print(ansArr)
+                return True
+            else:
+                return False
+        if i < len(arr):
+            ansArr.append(arr[i])
+            if self.find_subseq_sum_only_1(i + 1, arr,  total, ansArr):
+                return True
+            ansArr.pop()
+            if self.find_subseq_sum_only_1(i + 1, arr,  total, ansArr):
+                return True
+            return False
+
+    # return the count of total subsequence with the given sum
+    def find_subseq_sum_count(self, i, arr, total, ansArr=[]):
+        # print('+', ansArr)
+        if i >= len(arr):
+            if sum(ansArr) == total:
+                return 1
+            else:
+                return 0
+        if i < len(arr):
+            ansArr.append(arr[i])
+            l = self.find_subseq_sum_count(i + 1, arr,  total, ansArr)
+            ansArr.pop()
+            r = self.find_subseq_sum_count(i + 1, arr,  total, ansArr)
+            return l + r
+
 
 if __name__ == "__main__":
     x = Solution()
-    a = [3, 1, 2]
+    # a = [3, 1, 2]
+    a = [1, 2, 1]
     # x.solve_print(0, a, [])
-    x.solve_return(0, a, [])
-    x.solve_store_print()
+    # x.solve_return(0, a, [])
+    # x.solve_store_print()
+    # x.find_subseq_sum(0, a, 2, [])
+    # x.find_subseq_sum_only_1(0, a, 2, [])
+    # print(x.find_subseq_sum_count(0, a, 2, []))
 
-# LINKS https://www.youtube.com/watch?v=AxNNVECce8c&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=6
+# LINKS
+# https://www.youtube.com/watch?v=AxNNVECce8c&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=6
+# https://www.youtube.com/watch?v=eQCS_v3bw0Q&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=7
